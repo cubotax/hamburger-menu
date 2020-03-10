910cf7120c7d2ba6dc290ab95bbbba64547900d3
久保田泰寛 <cubotax@MBP2019.local>
2020-03-11 02:13:56 +0900

    質問を追加

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
-$(function(){
-	$(".btn-gnavi").on("click", function(){
+$(function () {
+	$(".btn-gnavi").on("click", function () {
 		// ハンバーガーメニューの位置を設定するための変数
 		var rightVal = 0;
-		if($(this).hasClass("open")) {
+		if ($(this).hasClass("open")) {
+			// 質問　class名にopenがあればrightValを0にすると書いてあるが、どこのopenを指定しているかわからない
+
+			// 質問　各所にあるthisはどこを指しているのか？
+
 			// 「open」クラスを持つ要素はメニューを開いた状態に設定
 			rightVal = -300;
 			// メニューを開いたら次回クリック時は閉じた状態になるよう設定
